6552f65b7aa92d4028cadde69dca96ad90aa0fc1
sonia <soniaummehabiba@gmail.com>
2019-10-10 17:44:11 +0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -60,7 +60,7 @@ router.post('/webhook', (req, res, next) => {
             .get(requestURL)
             .then(res => {
                 let {name, weather} = res.data;
-                return agent.add(`Its ${weather[0].main} (${weather.description})`)
+                return agent.add(`Its ${weather[0].main} (${weather[0].description})`)
             })
             .catch(err => {
                 console.log(`error in getting details: ${err}`);
